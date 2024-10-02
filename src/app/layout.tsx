6326d64e6fc39672d8df908133d5961ba249dcb5
Anjali Kurrewar <anjali.kurrewar1234@gmail.com>
2024-10-01 23:32:53 -0700

    implemented next auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import AuthProvider from '../context/AuthProviders';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,11 +29,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+       <AuthProvider>
+        <body className={inter.className}>
+          {children}
+        </body>
+      </AuthProvider>
     </html>
   );
 }
