040828fab3e5503be2ff99bf8fd35befcaefe667
Anjali Kurrewar <anjali.kurrewar1234@gmail.com>
2024-10-08 17:06:52 -0700

    done navbar

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import AuthProvider from '../context/AuthProviders';
+import AuthProvider from '../../context/AuthProviders';
 import { Inter } from 'next/font/google';
 import { Toaster } from "@/components/ui/toaster"
 
